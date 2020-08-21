99012d9fb552c3eb410376a4490303f032a9614b
nielsoffice <nieldigitalsolution@gmail.com>
2020-08-21 20:41:53 +0800

    update cpE saver 1.2 Official release

--- about.php
+++ about.php
@@ -1,7 +1,26 @@
-<?php require_once 'crownPHPEXTEND/crownPHPEXTEND.v.1.0.php';?>
 <?php 
-  
- 
 
+require_once 'crownPHPEXTEND/crownPHPEXTEND.v.1.0.php';
+
+/**
+*
+*--------------------------------------------------------------------------
+* @package Template: 
+* @since:  cpE v1.0 
+*--------------------------------------------------------------------------
+* PageName: Portfolio
+* DateCreated: 21.08.2020
+* Project Name: Demo
+* Page Description: This is for saver demo page v1.2
+* Page Tags: corePHP page named porfolio
+*
+**/
+
+?>
+
+<?php 
+ 
  get_SectionView('about-section-header');
+ 
  get_view('home');
+
